b0a93d5cfdeb93e1cdc749d4fc1a064c0692026f
Colo1396 <nazarenogalvan@outlook.com>
2020-06-01 15:22:12 -0300

    API-abmProductos

---
+++ abmProductos/src/main/java/com/unla/abmProductos/AbmProductosApplication.java
@@ -0,0 +1,13 @@
+package com.unla.abmProductos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbmProductosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbmProductosApplication.class, args);
+	}
+
+}
---
+++ abmProductos/src/main/java/com/unla/abmProductos/ProductoRepositorio.java
@@ -0,0 +1,12 @@
+package com.unla.abmProductos;
+
+import java.util.List;
+
+import org.springframework.data.repository.Repository;
+
+public interface ProductoRepositorio extends Repository<Producto,Integer>{
+	List<Producto>findAll();
+	Producto findById(int id);
+	Producto save(Producto p);
+	void delete(Producto p);
+}
